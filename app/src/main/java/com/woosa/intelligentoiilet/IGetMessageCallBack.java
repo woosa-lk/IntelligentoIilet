40ec60cb7d6fa2c5a333510388129efda9690c6f
刘可 <liuke@liukedeMacBook-Air.local>
2019-05-02 17:06:42 +0800

    base line.

---
+++ app/src/main/java/com/woosa/intelligentoiilet/IGetMessageCallBack.java
@@ -0,0 +1,5 @@
+package com.woosa.intelligentoiilet;
+
+public interface IGetMessageCallBack {
+        public void setMessage(String message);
+}
---
+++ app/src/main/java/com/woosa/intelligentoiilet/MainActivity.java
@@ -0,0 +1,43 @@
+package com.woosa.intelligentoiilet;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements IGetMessageCallBack{
+    private TextView text;
+    private MyServiceConnection serviceConnection;
+    private MQTTService mqttService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.textView);
+
+        serviceConnection = new MyServiceConnection();
+        serviceConnection.setIGetMessageCallBack(MainActivity.this);
+
+        Intent intent = new Intent(this, MQTTService.class);
+
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+        text.setText(R.string.click_text);
+
+    }
+
+    @Override
+    public void setMessage(String message) {
+        text.setText(message);
+        mqttService = serviceConnection.getMqttService();
+        mqttService.toCreateNotification(message);
+    }
+
+    @Override
+    protected void onDestroy() {
+        unbindService(serviceConnection);
+        super.onDestroy();
+    }
+}
